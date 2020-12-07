2fce95d666f90a0ddd8621c9da641860f19069e1
HaneulYun <yhnil.kr@gmail.com>
2020-12-07 19:17:56 +0900

    0.0.beta setting 4 lib 링크 설정

--- 2020cg_supporter/2020cg_supporter.cpp
+++ 2020cg_supporter/2020cg_supporter.cpp
@@ -1,9 +1,25 @@
 ﻿#include "pch.h"
 #include "Dependencies/glew.h"
 #include "Dependencies/freeglut.h"
-#include "Dependencies/glm/glm.hpp"
+//#include "Dependencies/glm/glm.hpp"
 
-int main()
+#include <iostream>
+
+int main(int argc, char** argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("Game Software Engineering KPU");
 	
+	glewInit();
+	if (glewIsSupported("GL_VERSION_3_0"))
+	{
+		std::cout << " GLEW Version is 3.0\n ";
+	}
+	else
+	{
+		std::cout << "GLEW 3.0 not supported\n ";
+	}
 }
