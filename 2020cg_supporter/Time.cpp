5336316108de3b64441fdc5b200fbbc09735372e
Meenki <meenki98@gmail.com>
2020-12-13 15:58:35 +0900

    0.0.12 Time

--- 2020cg_supporter/Time.cpp
+++ 2020cg_supporter/Time.cpp
@@ -3,11 +3,28 @@
 
 float Time::deltaTime = 0.0f;
 
+void Time::CalDeltaTime()
+{
+	deltaTime = std::chrono::duration<float>(mCurTime - mPreTime).count();
+
+	cout << deltaTime << "\n ";
+}
+
+void Time::SetCurTime()
+{
+	mCurTime = std::chrono::system_clock::now();
+}
+
+void Time::SetPreTime()
+{
+	mPreTime = std::chrono::system_clock::now();
+}
+
 void Time::Update()
 {
-	curTime = GetTickCount64();
+	SetCurTime();
 
-	deltaTime = (curTime - oldTime) / 1000.0f;
+	CalDeltaTime();
 
-	oldTime = curTime;
+	SetPreTime();
 }
